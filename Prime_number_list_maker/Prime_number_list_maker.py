47edac62c9974565ff0d8b3b31b6026ff7c46fd8
blue-w-potato <wame20080418@gmail.com>
2024-02-08 07:49:41 +0800

    hi

--- Prime_number_list_maker/Prime_number_list_maker.py
+++
@@ -1,9 +0,0 @@
-# https://zh.wikipedia.org/wiki/%E5%9F%83%E6%8B%89%E6%89%98%E6%96%AF%E7%89%B9%E5%B0%BC%E7%AD%9B%E6%B3%95
-def eratosthenes(n):
-    is_prime = [True] * (n + 1)
-    for i in range(2, int(n ** 0.5) + 1):
-        if is_prime[i]:
-            for j in range(i * i, n + 1, i):
-                is_prime[j] = False
-    return [x for x in range(2, n + 1) if is_prime[x]]
-print(eratosthenes(1000000))
