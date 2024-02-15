bbaf05fd4420ac0d756549e10be947cf1db10027
blue-w-potato <wame20080418@gmail.com>
2024-02-15 20:25:06 +0800

    new

--- simple_2048/main.py
+++ simple_2048/main.py
@@ -21,26 +21,39 @@ pygame.display.set_caption(screen_sets.name)
 clock = pygame.time.Clock()
 
 #載入圖片
-class PICTURES:
-    null = pygame.image.load(os.path.join('numbers', 'null.png')).convert()
-    num_2 = pygame.image.load(os.path.join('numbers', '2.png')).convert()
-    num_4 = pygame.image.load(os.path.join('numbers', '4.png')).convert()
-    num_8 = pygame.image.load(os.path.join('numbers', '8.png')).convert()
-    num_16 = pygame.image.load(os.path.join('numbers', '16.png')).convert()
-    num_32 = pygame.image.load(os.path.join('numbers', '32.png')).convert()
-    num_64 = pygame.image.load(os.path.join('numbers', '64.png')).convert()
-    num_128 = pygame.image.load(os.path.join('numbers', '128.png')).convert()
+
+LOAD_PICTURES = (
+    pygame.image.load(os.path.join('numbers', 'null.png')).convert(), 
+    pygame.image.load(os.path.join('numbers', '2.png')).convert(),
+    pygame.image.load(os.path.join('numbers', '4.png')).convert(),
+    pygame.image.load(os.path.join('numbers', '8.png')).convert(),
+    pygame.image.load(os.path.join('numbers', '16.png')).convert(),
+    pygame.image.load(os.path.join('numbers', '32.png')).convert(),
+    pygame.image.load(os.path.join('numbers', '64.png')).convert(),
+    pygame.image.load(os.path.join('numbers', '128.png')).convert(),
+            )
+
+class PICTURE:
+    def __init__(self, key, value, next) -> None:
+        self.key = key
+        self.value = value
+        self.next = next
+
+pictures = [ 'null', 2, 4, 8, 16, 32, 64, 128 ]
+pictures[0] = PICTURE( None, LOAD_PICTURES[0], None )
+for i in range( 1, len(LOAD_PICTURES) ):
+    pictures[i] = PICTURE( pictures[i-1], LOAD_PICTURES[i], None )
+    pictures[i-1].next = pictures[i]
+
 
 # 角色圖片設定
 class Player(pygame.sprite.Sprite): 
     def __init__(self, size, color, xy):
         pygame.sprite.Sprite.__init__(self) 
-        self.image = pygame.Surface(size) 
-        self.image.fill(color) 
-        self.rect = self.image.get_rect()
+        self.image = pictures[0] 
         self.x = screen_sets.width/2
         self.y = screen_sets.height/2
-        self.rect.center = [self.x, self.y]
+
 all_sprites = pygame.sprite.Group()
 
 #角色一相關設定
@@ -57,8 +70,9 @@ player_1 = Player(size = player_1_sets.size, color = player_1_sets.color,  xy = 
 all_sprites.add(player_1) 
 
 #遊戲主迴圈
-
+p = pygame.image.load(os.path.join('numbers', '2.png')).convert()
 running = True
+k = 0
 
 while running:
     clock.tick(screen_sets.fps) 
@@ -68,10 +82,15 @@ while running:
             running = False
 
     #更新遊戲
-    
+    if k < len(pictures)-1:
+        k += 1
+    else:
+        k = 0
+
     #畫面顯示
-    screen.fill(screen_sets.back_color) 
-    all_sprites.draw(screen) 
+    screen.blit(p , ( 0, 0 ))
+    screen.fill(screen_sets.back_color)
+    all_sprites.update() 
     pygame.display.update()
 
 pygame.quit() 
