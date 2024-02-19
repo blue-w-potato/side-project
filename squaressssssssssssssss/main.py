3b83eafb07ec57a75a18698d22890db3b56acc0e
blue-w-potato <wame20080418@gmail.com>
2024-02-19 22:09:07 +0800

    yeah

--- squaressssssssssssssss/main.py
+++ squaressssssssssssssss/main.py
@@ -1,19 +1,26 @@
 import turtle as t
+import time as mm
 t.colormode(255)
 t.shape('turtle')
 t.speed('fastest')
+
 def square(n):
     for i in range(4):
         t.forward(n)
         t.left(90)
+
 def main(n):
     while True:
-        #turn left 15
         square(n)
         t.penup()
-        t.left(180)
-        t.forward(round(n*0.08748866352, 20))#tan 5度
-        t.right(90)
+        t.left(90)
+        n2 = round(n*0.08748866352, 20)
+        t.forward(n2)
+        t.right(95)
         t.pendown()
-        n = round(n / 0.99619469809, 20)
-main(100)
+        n = round(((n - n2)**2 + n2**2)**0.5, 20)
+
+t.penup()
+t.goto(-300, -300)
+t.pendown()
+main(600)
