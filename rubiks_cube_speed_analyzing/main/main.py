3e3c3f218e1d290f527d34a9f2ca7dfad077b6c1
blue-w-potato <wame20080418@gmail.com>
2024-07-23 01:58:40 +0800

    r

---
+++ rubiks_cube_speed_analyzing/main/main.py
@@ -0,0 +1,4 @@
+# 建立csv檔
+import original_to_csv
+original_to_csv.main()
+
--- rubiks_cube_speed_analyzing/main/original_to_csv.py
+++ rubiks_cube_speed_analyzing/main/original_to_csv.py
@@ -1,4 +1,33 @@
 import os
 
-# 取得目錄中的檔案清單
-original_data = os.listdir( "main/original_data" )
+def main():
+    
+    # 取得目錄中的檔案清單
+    data_name = os.listdir( "main\original_data" )
+
+    # 讀取原始資料
+    original_data = open( encoding = "utf-8", mode = "r", file = "C:\\Users\\88690\\Desktop\\side-project\\rubiks_cube_speed_analyzing\\main\\original_data\\" + data_name[0] )
+    data = []
+    while 1:
+        try:
+            line = original_data.readline()
+            if line == "":
+                break
+            data.append( line[:-4] )
+        except EOFError:
+            break
+    original_data.close()
+    
+    # 處理資料
+    csv_data = []
+    for i in data:
+        if ";" in list(i):
+            csv_data.append( ",".join( i.split( ";" ) ) )
+        else:
+            csv_data.append( i )
+            
+    # 存檔
+    new_data = open( encoding = "utf-8", mode = "wt", file = "C:\\Users\\88690\\Desktop\\side-project\\rubiks_cube_speed_analyzing\\main\\csv_data\\original_data.csv" )
+    for i in csv_data:
+        print( i, file = new_data )
+    new_data.close()
